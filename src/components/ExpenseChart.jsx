ea4cebe57e41fbaf8456757b5d59fd50ea4f2563
simon0820s <simon0820s@gmail.com>
2023-08-07 13:18:45 -0500

    upgrade grafic

--- src/components/ExpenseChart.jsx
+++ src/components/ExpenseChart.jsx
@@ -1,55 +1,43 @@
 import { VictoryPie, VictoryLabel } from 'victory'
 import { useGlobalState } from '../context/GlobalState'
+import { useEffect, useState } from 'react'
 
 const ExpenseChart = () => {
 
   const { transactions } = useGlobalState()
+  const [income, setIncome] = useState(100)
+  const [expense, setExpense] = useState(0)
 
-  const totalIncome = transactions.filter((transaction) => transaction.amount > 0).reduce((acc, transaction) => (acc += transaction.amount), 0)
+  useEffect(() => {
 
-  const totalExpense = transactions.filter((transaction) => transaction.amount < 0).reduce((acc, transaction) => (acc += transaction.amount), 0) * -1
+    const totalIncome = transactions.filter((transaction) => transaction.amount > 0).reduce((acc, transaction) => (acc += transaction.amount), 0)
 
-  const expensePercentage = (totalExpense / totalIncome) * 100
-  const roundedExpensePercentage = expensePercentage.toFixed(2)
-  const incomePercentage = 100 - roundedExpensePercentage
+    const totalExpense = transactions.filter((transaction) => transaction.amount < 0).reduce((acc, transaction) => (acc += transaction.amount), 0) * -1
 
-  if (totalIncome > 0) {
-    return (
+    const expensePercentage = Math.round((totalExpense / totalIncome) * 100)
+    setExpense(expensePercentage) 
+    setIncome(100 - expensePercentage)
 
-      <VictoryPie
-        colorScale={["#cc2649", "#66324c"]}
-        data={[
-          { x: "Expense", y: roundedExpensePercentage },
-          { x: "Income", y: incomePercentage },
-        ]}
-        animate={{
-          duration: 200
-        }}
-        labels={({ datum }) => `${datum.y}%`}
-        labelComponent={<VictoryLabel
-          angle={45}
-          style={{
-            fill: "red",
-            fontSize: 17
-          }} />} />
-    )
-  } else {
-    return (
-      <VictoryPie
-        colorScale={["#cc2649", "#66324c"]}
-        data={[
-          { x: "Expense", y: 50 },
-          { x: "Income", y: 50 },
-        ]}
-        labels={({ datum }) => `${datum.y}%`}
-        labelComponent={<VictoryLabel
-          angle={45}
-          style={{
-            fill: "red",
-            fontSize: 17
-          }} />} />
-    )
-  }
+  }, [transactions])
+
+  return (
+    <VictoryPie
+      colorScale={["#cc2649", "#66324c"]}
+      data={[
+        { x: "Expense", y: expense },
+        { x: "Income", y: income },
+      ]}
+      animate={{
+        duration: 200
+      }}
+      labels={({ datum }) => `${datum.y}%`}
+      labelComponent={<VictoryLabel
+        angle={45}
+        style={{
+          fill: "red",
+          fontSize: 17
+        }} />} />
+  )
 }
 
 export default ExpenseChart
